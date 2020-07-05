0bcd3bbc5ed39d942aa568729f17d128376ea9fb
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2020-07-05 14:19:04 +0600

    basic setup with PSR-4

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap ">
+		<a class="btn btn-info" href="all_data.php">show all</a>
+		<div class="card shadow">
+			<div class="card-body">
+				<h2>Sign Up</h2>
+				<form action="<?php echo $_SERVER['PHP_SELF']?>">
+					<div class="form-group">
+						<label for="">Name</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Email</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Cell</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Username</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<input class="btn btn-primary" type="submit" value="Sign Up">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
